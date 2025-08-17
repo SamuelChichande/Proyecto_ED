8760360f702aedac8429fc087a374dc14a92eeb0
MoreiraC18 <cesmchav@espol.edu.ec>
2025-08-17 10:57:06 -0500

    Actualizacion Ventana Inicio

--- Proyecto_ED/app/build.gradle.kts
+++ Proyecto_ED/app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
+
+    implementation("com.airbnb.android:lottie:6.6.6")
 }
