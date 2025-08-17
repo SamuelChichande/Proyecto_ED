34bb519316581350b67f76bb9114ddeffb5539e4
JerePoveda <povedajeremias27@gmail.com>
2025-08-17 03:23:07 -0500

    inicio y registro casi terminados

--- Proyecto_ED/app/build.gradle.kts
+++ Proyecto_ED/app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -40,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.airbnb.android:lottie:6.6.7")
+
 }
